b81430f6d253e1c32fb8953666afc1b116cf37bd
Leo Furze-Waddock <leo@furze-waddock.com>
2026-01-12 20:33:42 +0000

    Revert "fix ci"
    This reverts commit fe48def2ef5b1707d8792f84248f448314ececf4.

--- packages/coordinates/src/index.ts
+++ packages/coordinates/src/index.ts
@@ -4,6 +4,5 @@ export * from "./CurrentCoordinate";
 export * from "./Cursor";
 export * from "./MouseCoordinateX";
 export { MouseCoordinateXV2 } from "./MouseCoordinateXV2";
-export { MouseCoordinateY, getYCoordinate } from "./MouseCoordinateY";
+export { MouseCoordinateY } from "./MouseCoordinateY";
 export * from "./PriceCoordinate";
-export { drawOnCanvas } from "./EdgeCoordinateV3";
