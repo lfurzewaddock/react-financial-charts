fe48def2ef5b1707d8792f84248f448314ececf4
Leo Furze-Waddock <leo@furze-waddock.com>
2026-01-12 20:13:50 +0000

    fix ci

--- packages/coordinates/src/index.ts
+++ packages/coordinates/src/index.ts
@@ -4,5 +4,6 @@ export * from "./CurrentCoordinate";
 export * from "./Cursor";
 export * from "./MouseCoordinateX";
 export { MouseCoordinateXV2 } from "./MouseCoordinateXV2";
-export { MouseCoordinateY } from "./MouseCoordinateY";
+export { MouseCoordinateY, getYCoordinate } from "./MouseCoordinateY";
 export * from "./PriceCoordinate";
+export { drawOnCanvas } from "./EdgeCoordinateV3";
